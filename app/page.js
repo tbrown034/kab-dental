a555708807f3e3d0f12ed9a678f9fc1d7e5abe21
tbrown034 <tbrown034@gmail.com>
2023-05-15 11:43:20 -0500

    added react-icons

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { FaTooth } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div>
+      <h3>
+        <FaTooth />
+      </h3>
+    </div>
+  );
+}
